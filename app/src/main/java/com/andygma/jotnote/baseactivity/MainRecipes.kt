55074841ad4146c9dc7943095101f7e0353bc5a3
Andrew <555135@mail.ru>
2023-05-03 14:05:12 +0300

    Добавил поля для ингридиентов

--- app/src/main/java/com/andygma/jotnote/baseactivity/MainRecipes.kt
+++ app/src/main/java/com/andygma/jotnote/baseactivity/MainRecipes.kt
@@ -7,16 +7,22 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Clear
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -29,12 +35,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.andygma.jotnote.R
 import com.andygma.jotnote.resProject.TextJot
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 fun MainRecipes() {
     Column(modifier = Modifier
@@ -104,35 +111,81 @@ private fun Content() {
             .fillMaxWidth()
     ) {
         Box(
-            modifier = Modifier.fillMaxWidth().background(Color.Green),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(5.dp)
+                .padding(top = 5.dp),
             contentAlignment = Alignment.Center
         ) {
             Text(text = "Название блюда")
         }
         Box(
-            modifier = Modifier.fillMaxWidth().height(60.dp)
+            modifier = Modifier
+                .fillMaxWidth()
         ) {
-            Row() {
-                Card(
-                    modifier = Modifier
-                        .fillMaxWidth(0.9f)
-                        .background(Color.Blue),
-                    shape = RoundedCornerShape(20.dp),
-                    elevation = CardDefaults.cardElevation(
-                        defaultElevation = 10.dp
-                    ),
-                    border = BorderStroke(1.dp, Color.Blue)
-
-                ) {
-                    Text(text = "Hello")
-                }
+            Row {
+                CardJot()
                 Box(modifier = Modifier
                     .fillMaxWidth()
-                    .background(Color.Red)
+                    .background(Color.Gray)
                 ) {
-                    Text(text = "Hello")
+                    Text(text = "Добавить", modifier = Modifier.fillMaxSize())
                 }
             }
+        }
+    }
+}
+
+@Composable
+private fun CardJot() {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth(0.9f),
+        shape = RoundedCornerShape(20.dp),
+        border = BorderStroke(2.dp, color = colorResource(id = R.color.colorPrimary))
+
+    ) {
+        Text(text = "Ингридиенты",
+            modifier = Modifier
+                .padding(5.dp)
+                .fillMaxWidth(),
+            textAlign = TextAlign.Center
+        )
+        Ingrid()
+        Ingrid()
+        Ingrid()
+        Ingrid()
+    }
+}
+
+@Composable
+private fun Ingrid() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(40.dp)
+    ) {
+        Card(
+            modifier = Modifier
+                .fillMaxWidth(0.9f)
+                .fillMaxHeight()
+                .height(30.dp)
+                .padding(5.dp)
+                .padding(start = 10.dp),
+            elevation = CardDefaults.cardElevation(
+                defaultElevation = 10.dp
+            )
+        ) {
+            Text(text = "Ингридиенты #1",
+                Modifier
+                    .padding(5.dp)
+            )
+        }
+        IconButton(
+
+            onClick = { /* doSomething() */ }
+        ) {
+            Icon(Icons.Outlined.Clear, contentDescription = "Localized description", modifier = Modifier.height(20.dp))
         }
     }
 }
